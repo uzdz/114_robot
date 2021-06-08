31912a40cc5c5d153d3e9732afb82a6703a27b3d
Zhen Dong <devmen@163.com>
2021-06-08 16:56:32 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# 114预约挂号平台实时刷新工具
+
+#### 1. 自主登陆114获取cookie信息
+
+* 114网址：https://www.114yygh.com/
+
+登陆到114之后，自主进行授权操作，登陆后，点击F12进入开发者工具。
+
+![](images/F9D7620F-E6D6-430C-A419-9700181294CC.png)
+
+找到Application选项卡，然后找到cookie为`cmi-user-ticket`的cookie，将Key和Value通过等于号(=)拼装起来拼。运行程序第一步键入该值！
+
+![](images/40D69962-D690-4CE7-BAB2-5524E966A724.png)
+
+#### 2. 选择要实时检测的门诊网址
+
+选择自己希望查询的门诊科室，然后复制URL即可，例如：
+
+* https://www.114yygh.com/hospital/126/c1df24b5f98d8dc660112aa1f81f24e4/200000264/source
+* https://www.114yygh.com/hospital/122/d3bbb5cb1ac8a26829dd4e23b88f839a/200044316/source
+* https://www.114yygh.com/hospital/3/57c6654fe6c60748d71f02ffacfadd1e/200047992/source
+
+必须点击到最细的科室级别，然后复制URL，否则无发进行解析。
+
+![](images/6D65B0A1-8980-4AEF-8596-C4AEB7FB87E4.png)
+
+#### 3. 运行py文件
+
+![](images/A110686C-3D4D-4603-8687-C6F476A0AEF8.png)
+
+#### 4. 实时查看门诊预约状态（每30秒查询一次）
+
+![](images/60D988FB-BCAC-47D1-B009-5553D4646A98.png)
+
+## 请不要加快查询速度，防止对114服务造成请求压力，维护计算机安全。
---
+++ robot.py
@@ -0,0 +1,197 @@
+import requests
+import json
+import time
+from rich.table import Table
+from rich.console import Console
+from rich import box
+from rich.live import Live
+import datetime
+from datetime import timedelta
+from rich.layout import Layout
+from rich.panel import Panel
+
+headers = {
+    'Content-Type': 'application/json',
+    'Request-Source': 'PC',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6)'
+                  ' AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36'
+}
+
+
+def parsing_url(url: str) -> dict:
+    """
+    解析所需解析门诊地址，强制要求满足格式
+
+    https://www.114yygh.com/hospital/162/10c186f26ae7ecf8160e2dcf1f2e7312/200053566/source
+    {"hosCode": url_split[4], "firstDeptCode": url_split[5], "secondDeptCode": url_split[6]}
+
+    :param url: 门诊地址
+    :return:
+    """
+
+    url_split = url.split("/")
+    if len(url_split) != 8:
+        raise Exception("门诊解析地址无效！请核对。", url)
+    else:
+        md = {}
+        headers["Referer"] = url
+
+        hos_code = url_split[4]
+        first_dept_code = url_split[5]
+        second_dept_code = url_split[6]
+
+        week_os_info = request_week_os_info(first_dept_code, second_dept_code, hos_code)
+        md.update(week_os_info)
+
+        os_base_properties = request_os_properties(first_dept_code, second_dept_code, hos_code)
+        md.update(os_base_properties)
+
+        return md
+
+
+def request_week_os_info(first_dept_code: str, second_dept_code: str, hos_code: str) -> dict:
+    """
+    获取当前一星期的预约状况
+
+    :param first_dept_code: first_dept_code
+    :param second_dept_code: second_dept_code
+    :param hos_code: hos_code
+    :return:
+    """
+
+    body = {
+        "firstDeptCode": first_dept_code,
+        "secondDeptCode": second_dept_code,
+        "hosCode": hos_code,
+        "week": 1
+    }
+
+    request_url = "https://www.114yygh.com/web/product/list"
+    response = requests.post(request_url, headers=headers, data=json.dumps(body)).json()
+
+    if response["resCode"] != 0:
+        raise Exception("获取门诊预约信息异常，请前往检查！", request_url, body, response)
+
+    return response["data"]
+
+
+def request_os_properties(first_dept_code: str, second_dept_code: str, hos_code: str) -> dict:
+    """
+    通过请求地址获取门诊信息
+
+    :param first_dept_code: first_dept_code
+    :param second_dept_code: second_dept_code
+    :param hos_code: hos_code
+    :return:
+    """
+
+    format_url = "https://www.114yygh.com/web/department/hos/detail?firstDeptCode={}&secondDeptCode={}&hosCode={}"
+
+    request_url = format_url.format(first_dept_code, second_dept_code, hos_code)
+    response = requests.get(request_url, headers=headers).json()
+
+    if response["resCode"] != 0:
+        raise Exception("获取门诊基础信息异常，请检查！", request_url, response)
+
+    return response["data"]
+
+
+def parsing_url_with_list(urls: list) -> list:
+    """
+    将多个门诊地址进行解析并返回
+
+    :param urls: 门诊集合列表
+    :return:
+    """
+
+    os_parsed_list = []
+    for url in urls:
+        os_parsed_list.append(parsing_url(url))
+
+    return os_parsed_list
+
+
+def all_info_of_table(request_os_list: list) -> Table:
+    parsed_data = parsing_url_with_list(request_os_list)
+
+    # 最近一周的日期 %Y-%m-%d
+    week_of_name = []
+    now = datetime.datetime.now()
+    for value in range(1, 8):
+        next_day = now + timedelta(days=value)
+        week_of_name.append(next_day.strftime("%Y-%m-%d"))
+
+    table = Table(box=box.ROUNDED, title="[aquamarine3]114 网上预约实时监控({})".format(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))))
+
+    table.add_column('[light_sea_green]医院', justify="center")
+    table.add_column('[light_sea_green]科部', justify="center")
+    table.add_column('[light_sea_green]门诊', justify="center")
+
+    for value in week_of_name:
+        table.add_column('[light_sea_green]' + value, justify="center")
+
+    for data in parsed_data:
+
+        week_of_dict = {0: "[red]未知", 1: "[red]未知", 2: "[red]未知", 3: "[red]未知", 4: "[red]未知", 5: "[red]未知",
+                        6: "[red]未知"}
+
+        for index, value in enumerate(week_of_name):
+            for calendars in data["calendars"]:
+                if value == calendars["dutyDate"]:
+                    if calendars["status"] == "NO_INVENTORY":
+                        # 无号
+                        week_of_dict[index] = "[red3]无号"
+                    elif calendars["status"] == "AVAILABLE":
+                        # 可约
+                        week_of_dict[index] = "[green]可预约"
+                    elif calendars["status"] == "SOLD_OUT":
+                        # 约满
+                        week_of_dict[index] = "[indian_red]已约满"
+                    elif calendars["status"] == "TOMORROW_OPEN":
+                        # 即将放号
+                        week_of_dict[index] = "[steel_blue1]即将放号"
+                    else:
+                        week_of_dict[index] = "[red]未知状态"
+                    break
+
+        table.add_row(data["hosName"], data["firstDeptName"], data["secondDeptName"],
+                      week_of_dict[0], week_of_dict[1],
+                      week_of_dict[2], week_of_dict[3],
+                      week_of_dict[4], week_of_dict[5],
+                      week_of_dict[6])
+
+    return table
+
+
+if __name__ == '__main__':
+    console = Console(color_system='256', style=None)
+
+    cookie = console.input(":surfer:[bold deep_sky_blue3] 请输入114北京市预约挂号统一平台授权凭证Cookie：\n")
+    headers["Cookie"] = cookie
+
+    os_list = []
+
+    url = console.input(":robot:[bold sky_blue2] 请键入要实时查询的门诊地址：\n")
+    os_list.append(url)
+
+    while True:
+        access_or_url = console.input(":heavy_exclamation_mark:[bold red1] 键入[Y]进行查询，否则继续批量录入门诊URL：\n")
+        if access_or_url == "Y":
+            break
+        else:
+            os_list.append(access_or_url)
+
+    os_data = None
+    with console.status("[light_goldenrod3]正在首次加载数据...[/]", spinner="moon"):
+        os_data = all_info_of_table(os_list)
+
+    # 首次加载由外部完成，normal为正常加载sleep 5s
+    normal = False
+    with Live(console=console, screen=True, auto_refresh=False) as live:
+        while True:
+            if normal:
+                time.sleep(5)
+                os_data = all_info_of_table(os_list)
+
+            live.update(os_data, refresh=True)
+            normal = True
